2bc94878d44ce316e558554950f6e7856e851498
Simone Beaver <simone.beaver@verizon.com>
2019-07-12 09:07:44 -0500

    its working here

--- js/Game.js
+++ js/Game.js
@@ -5,15 +5,18 @@ class Game { //Here the game class is declared.
     constructor() { // The constructor method was created here with the properties missed, phrases, and activePhrase.
         this.missed = 0;
         this.phrases = [
-             {'phrase': "I\'m walking on sunshine"},
-             {'phrase':"Gimme the loot"},
-             {'phrase':"Stop \, Drop\, Roll"},
-             {'phrase':"Show me the money"},
-             {'phrase':"It all gets better with time"}
+             new Phrase("I\'m walking on sunshine"),
+             new Phrase("Gimme the loot"),
+             new Phrase("Stop \, Drop\, Roll"),
+             new Phrase("Show me the money"),
+             new Phrase("It all gets better with time")
 
         ];
         this.activePhrase ="null";
         
     }
-
+    getRandomPhrase() {
+        let phraseIndex = Math.floor(Math.random() * (this.phrases.length));
+        return this.phrases[phraseIndex];
+      }
 }
--- js/app.js
+++ js/app.js
@@ -2,9 +2,18 @@
  * Project 4 - OOP Game App
  * app.js */
 // const phrase = new Phrase();
-// const game = new Game();
+
 //   let phrase = new Phrase('I\'m walking on sunshine');
 
 // game.phrases.forEach((phrase, index )=> {
 //     console.log(`Phrase ${index} -phrase: ${phrase.phrase}`);
 // });
+const logPhrase = (phrase) => {
+    console.log(`Phrases - phrases: `, phrase.phrase);
+};
+const game = new Game();
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
